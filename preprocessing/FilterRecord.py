c979595eb808f5511a504c5992986429b35bee26
Yuting Zhang <lizlingng@gmail.com>
2016-04-14 00:30:41 -0700

    Add function to get the total number of records that meet the requirements

--- preprocessing/FilterRecord.py
+++ preprocessing/FilterRecord.py
@@ -4,6 +4,9 @@
 import json
 import codecs
 
+"""
+	Return a dictionary of urls that are restaurants
+"""
 def construct_url():
 	fin = open("businesses.txt", 'r')
 	types = [u'太仓市', u'西樵', u'更多购物场所', u'博览中心']
@@ -26,12 +29,34 @@ def construct_url():
 					urls[url] = 1
 	return urls
 
-def generate_filtered_tmp(urldic):
-	
+def get_records_num(urls):
+	total_records_num = 0
+	fin = open("reviews.txt", 'r')
+	while 1:
+		line = fin.readline()
+		if not line:
+			break
+		if "flavor\":-1" in line:
+			continue
+		if "environment\":-1" in line:
+			continue
+		if "service\":-1" in line:
+			continue
+		if "content\":\"\"" in line:
+			continue
+		url = line.split(" ^ {")[0]
+		if urls.has_key(url):
+			total_records_num += 1
+	return total_records_num
+
+def split_files(file_num):
+	num_per_file = 3400000 / file_num
+
 
 def main():
     urls_dic = construct_url()
-    generate_filtered_tmp(urls_dic)
+    # print get_records_num(urls_dic)    #3363141
+    split_files(100)
 
 if __name__ == "__main__":
     main()
